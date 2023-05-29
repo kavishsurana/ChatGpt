3a5c723b75c5c05d22f89a4e64afe9396e66d304
Kavish <suranakavish@gmail.com>
2023-05-29 15:31:03 +0530

    Updated Code

--- client/src/api.js
+++ client/src/api.js
@@ -1,30 +1,3 @@
-// export const fetchResponse = async(chat) => {
-//     try {
-//         console.log(message)
-//         // console.log(chat)
-//         const temp = []
-//         const response = await fetch('http://localhost:3080/' , {
-//             method: 'POST',
-//             headers: {
-//                 "Content-Type": "application/json"
-//             },
-//             body: JSON.stringify({
-                
-//                 messages: chat.map((message) =>console.log(chat)).json("\n")
-//             })
-            
-            
-//         })
-
-//         const data = await response.json()
-//         return data
-//     } catch (err) {
-//         console.log(err)
-//     }
-// }
-
-
-
 
 export const fetchResponse =  async(chat) => {
     try {
